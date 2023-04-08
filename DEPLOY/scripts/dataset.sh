0ca57009e6065d28af9bc1d908f08aa2d77d430c
zoohoson <zoohoson@gmail.com>
2023-04-08 19:45:29 +0900

    code

--- DEPLOY/scripts/dataset.sh
+++ DEPLOY/scripts/dataset.sh
@@ -1,5 +1,5 @@
 python ../dbydeep_data.py \
-    --save-path /data/2021_SJH_detectability/diff_tool/human_hek293/ \
-    --protein-fasta /data/2021_SJH_detectability/diff_tool/sprot_human_220920.fasta \
-    --peptide-tsv /data/2021_SJH_detectability/diff_tool/H.sapiens_HEK293[21]_msgf_Fdr1.tsv \
-    --tool-name msgfplus >../../revision/log/log_hek293.out &
+    --save-path data/save/path/ \
+    --protein-fasta /data/path/protein.fasta \
+    --peptide-tsv /data/path/search_result.tsv \
+    --tool-name msgfplus
--- DEPLOY/scripts/model.sh
+++ DEPLOY/scripts/model.sh
@@ -1,7 +1,6 @@
-nohup python ../dbydeep_model.py \
+python ../dbydeep_model.py \
     --retrain-flag True \
-    --data-path /data/2021_SJH_detectability/data_cross_species/raw/mouse/KLife/KLife.csv \
-    --model-path /home/bis/2021_SJH_detectability/DbyDeep/log/model_DbyDeep_04_False.h5 \
+    --data-path ../data/test.csv \
+    --model-path ../data/DbyDeep.h5 \
     --save-path ../data/ \
-    --job-name DbyDeep_retrained \
-     >../../revision/log/result_prediction_gru.out &
+    --job-name DbyDeep_retrained
